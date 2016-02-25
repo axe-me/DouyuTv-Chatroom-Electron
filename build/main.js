54ebc3774c321304655122925e3ff30584e27fe6
ShaneXie <shane369@gmail.com>
2016-02-25 16:48:41 -0600

    nam warning is annoying

--- build/main.js
+++ build/main.js
@@ -24,7 +24,7 @@ app.on('ready', function () {
   mainWindow.loadUrl('file://' + __dirname + '/index.html');
 
   // Open the devtools.
-  mainWindow.openDevTools();
+  //mainWindow.openDevTools();
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
